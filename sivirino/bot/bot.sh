ca170c8dc3ca5c732f4e586c8b951f9043df2221
pedro <pkovokar@gmail.com>
2025-04-09 19:37:26 -0300

    commit 1

--- sivirino/bot/bot.sh
+++ sivirino/bot/bot.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Caminho para o arquivo JS
 ARQUIVO_JS="now.js"
 
 # Verifica se o arquivo existe
-if [ -f "$ARQUIVO_JS" ]; then
-    echo "Executando $ARQUIVO_JS com Node.js..."
-    node "$ARQUIVO_JS" > now.txt
-    git add . && git commit -m "Atualização automática"
-else
+if [ ! -f "$ARQUIVO_JS" ]; then
     echo "Arquivo $ARQUIVO_JS não encontrado!"
+    exit 1
 fi
+
+for i in {1..10}
+do
+    echo "Executando $ARQUIVO_JS - Volta $i"
+    node "$ARQUIVO_JS" > now.txt
+    git add .
+    git commit -m "commit $i"
+done
