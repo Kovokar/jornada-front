10deff655d3740b80153c8ef1f0d3be8f6e2015d
pedro <pkovokar@gmail.com>
2025-04-09 19:34:25 -0300

    Atualização automática

---
+++ sivirino/bot/bot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Caminho para o arquivo JS
+ARQUIVO_JS="now.js"
+
+# Verifica se o arquivo existe
+if [ -f "$ARQUIVO_JS" ]; then
+    echo "Executando $ARQUIVO_JS com Node.js..."
+    node "$ARQUIVO_JS"
+    git log --oneline
+    git add . && git commit -m "Atualização automática"
+else
+    echo "Arquivo $ARQUIVO_JS não encontrado!"
+fi
