c31a4f668c5832a60fd9c0163cf8b042812484f8
kovokar <pkovokar@gmail.com>
2026-03-11 20:59:05 -0300

    feat: firts pages

--- react/movies/src/main.tsx
+++ react/movies/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home.tsx'
+import { Movie } from './pages/Movie.tsx'
+import { Search } from './pages/Search.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+          <Route path='movie/:id' element={<Movie />} />
+          <Route path='search' element={<Search />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
