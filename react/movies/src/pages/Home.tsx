cc38b0fd157c6fb65fdf2479647ce72d0ec40d8f
kovokar <pkovokar@gmail.com>
2026-03-12 12:34:43 -0300

    feat: struct home

--- react/movies/src/pages/Home.tsx
+++ react/movies/src/pages/Home.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+type Movie = {
+    id: number
+    title: string
+    poster_path: string
+    vote_average: number
+};
+
 export const Home = () => {
+    const [topMovies, setTopMovies] = useState<Movie[]>([]);
+
+    const getTopRatedMovies = async (url: string): Promise<void> => {
+        const res = await fetch(url);
+        const data = await res.json();
+        setTopMovies(data.results);
+    };
+
+    useEffect(() => {
+        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+        getTopRatedMovies(topRatedUrl);
+    }, []);
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="container">
+            <h2 className="title">Melhores filmes:</h2>
+            <div className="movies-container">
+                {topMovies.length > 0 &&
+                    topMovies.map((movie) => (
+                        <MovieCard key={movie.id} movie={movie} />
+                    ))}
+            </div>
         </div>
-    )
-}
+    );
+};
